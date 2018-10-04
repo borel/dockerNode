0b44b361440f9aefc06f0d6d959a75d5e433deec
pbborel <pbborel@gmail.com>
2018-10-04 17:14:25 +0200

    Add mongo-db mgmt

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Constants
 const PORT = 8080;
@@ -6,12 +7,24 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// Db connect
+mongoose.connect('mongodb://mongo:27017' , {
+  useMongoClient: true
+}); 
+
+// Build model
+const HttpCall = mongoose.model('httpCall', { name: String , time: String});
+
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
 app.get('/hello', (req, res) => {
-  console.log('req.query =>', req.query)
+  // Save call in db
+  const  httpCal = new HttpCall({ name: req.query.name, time: Date.now()});
+  httpCal.save().then(() => console.log(`save ${req.query.name} in db`));
+
   res.send(`Hello ${req.query.name}\n`);
 });
 
